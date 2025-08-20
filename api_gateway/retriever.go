2ced8875fb73d45069175ed1d367d2ae207beca3
enjoykanyu <creatkanyu@outlook.com>
2025-08-20 22:03:18 +0800

    feat：向量数据库检索retriever

---
+++ api_gateway/retriever.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cloudwego/eino-ext/components/embedding/ark"
+	"github.com/milvus-io/milvus-sdk-go/v2/client"
+
+	"github.com/cloudwego/eino-ext/components/retriever/milvus"
+)
+
+func main() {
+	// Get the environment variables
+	addr := os.Getenv("MILVUS_ADDR")
+	username := os.Getenv("MILVUS_USERNAME")
+	password := os.Getenv("MILVUS_PASSWORD")
+	arkApiKey := os.Getenv("ARK_API_KEY")
+	arkModel := os.Getenv("ARK_MODEL")
+
+	// Create a client
+	ctx := context.Background()
+	cli, err := client.NewClient(ctx, client.Config{
+		Address:  addr,
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+		return
+	}
+	defer cli.Close()
+
+	// Create an embedding model
+	emb, err := ark.NewEmbedder(ctx, &ark.EmbeddingConfig{
+		APIKey: arkApiKey,
+		Model:  arkModel,
+	})
+
+	// Create a retriever
+	retriever, err := milvus.NewRetriever(ctx, &milvus.RetrieverConfig{
+		Client:      cli,
+		Collection:  "",
+		Partition:   nil,
+		VectorField: "",
+		OutputFields: []string{
+			"id",
+			"content",
+			"metadata",
+		},
+		DocumentConverter: nil,
+		MetricType:        "",
+		TopK:              0,
+		ScoreThreshold:    5,
+		Sp:                nil,
+		Embedding:         emb,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create retriever: %v", err)
+		return
+	}
+
+	// Retrieve documents
+	documents, err := retriever.Retrieve(ctx, "milvus")
+	if err != nil {
+		log.Fatalf("Failed to retrieve: %v", err)
+		return
+	}
+
+	// Print the documents
+	for i, doc := range documents {
+		fmt.Printf("Document %d:\n", i)
+		fmt.Printf("title: %s\n", doc.ID)
+		fmt.Printf("content: %s\n", doc.Content)
+		fmt.Printf("metadata: %v\n", doc.MetaData)
+	}
+}
