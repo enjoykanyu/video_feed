d764426e5f4030ee485f1e562cd435e7c37a8f9e
enjoykanyu <creatkanyu@outlook.com>
2025-08-21 23:32:34 +0800

    feat：拆分下embedding

---
+++ api_gateway/embedding.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/cloudwego/eino-ext/components/embedding/ollama"
+	"github.com/cloudwego/eino/components/embedding"
+	"log"
+	"os"
+	"time"
+)
+
+func embeddingTest() {
+	ctx := context.Background()
+
+	baseURL := os.Getenv("OLLAMA_BASE_URL")
+	if baseURL == "" {
+		baseURL = "http://localhost:11434" // 默认本地
+	}
+	model := os.Getenv("OLLAMA_EMBED_MODEL")
+	if model == "" {
+		model = "nomic-embed-text"
+	}
+
+	embedder, err := ollama.NewEmbedder(ctx, &ollama.EmbeddingConfig{
+		BaseURL: baseURL,
+		Model:   model,
+		Timeout: 10 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("NewEmbedder of ollama error: %v", err)
+		return
+	}
+
+	log.Printf("===== call Embedder directly =====")
+
+	vectors, err := embedder.EmbedStrings(ctx, []string{"hello", "how are you"})
+	if err != nil {
+		log.Fatalf("EmbedStrings of Ollama failed, err=%v", err)
+	}
+
+	log.Printf("vectors : %v", vectors)
+
+	// you can use WithModel to specify the model
+	vectors, err = embedder.EmbedStrings(ctx, []string{"hello", "how are you"}, embedding.WithModel(model))
+	if err != nil {
+		log.Fatalf("EmbedStrings of Ollama failed, err=%v", err)
+	}
+
+	log.Printf("vectors : %v", vectors)
+}
